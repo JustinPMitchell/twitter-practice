52b5d25476e465465732f5cd28f0edf6b13498d3
JustinPMitchell <j.mitchell.real@gmail.com>
2018-03-18 15:43:29 -0700

    updated config file

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'VAi0MMrBMbMu6iNYgficEZP2O',
+  consumer_secret:      'CBsALGHFdD5lsVjwZH1gSHzXlBMdFQJLIA1wMsAQjbTX9FuGCh',
+  access_token:         '	975499455108497408-BvojU8YhF9GAkoX2rthsbnenbItSybn',
+  access_token_secret:  'EVhABpSgbFXMDpCVsFx9kGdpjQfFwhaEVBTai6UmKG1x5',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
